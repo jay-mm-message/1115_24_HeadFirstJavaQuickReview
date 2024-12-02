c06ef8e9d452adb093505fbf7793558b099fc2c4
jay-mm-message <jay.mm.message@gmail.com>
2024-12-03 04:14:32 +0800

    Refactor and add commands

--- ch05/SimpleDotCom/SimpleDotComTestDrive.java
+++ ch05/SimpleDotCom/SimpleDotComTestDrive.java
@@ -3,7 +3,12 @@ package ch05.SimpleDotCom;
 import java.util.Scanner;
 
 public class SimpleDotComTestDrive {
+    public static void prompt(String s) {
+        System.out.print(s);
+    }
+
     public static void main(String[] args) {
+
         /*
          * Extreme Programming
          * SimpleDotCom dot = new SimpleDotCom();
@@ -11,6 +16,29 @@ public class SimpleDotComTestDrive {
          * dot.setLocationCells(locations);
          * String userGuess = "2";
          * String result = dot.checkYourself(userGuess);
+         */
+
+        /*
+         *  Declare int [] locationCells = new locationCells[3];
+         * 
+         *  randomly draw three numbers from 0 to 5 and
+         *  put them into locationCells.
+         *  for(int i = 0 ; i < locationCells.length ; ++i) {
+         *      locationCells[i] = (int) Math.random() * 5;
+         *  }
+         *  Declare:  SimpleDotCom dot = new SimpleDotCom();
+         *      set:    dot.setLocationCells(locationCells);
+         *  
+         *  String shipAlive;
+         *  try (Scanner sc = new Scanner(System.in)) {
+         *      do {
+         *          prompt("user guess:");
+         *          shipAlive = dot.checkYourself(sc.nextLine());
+         *      } while (ship is killed);
+         *  } catch(Exception e) {
+         *      prompt(e);
+         *  }
+         *
          */
 
         SimpleDotCom simpleDotCom = new SimpleDotCom();
@@ -22,15 +50,16 @@ public class SimpleDotComTestDrive {
         String result;
         try (Scanner scanner = new Scanner(System.in)) {
             do {
-                System.out.print("Enter a number: ");
+                prompt("Enter a number: ");
                 String userGuess = scanner.nextLine();
                 result = simpleDotCom.checkYourself(userGuess);
-                System.out.print("Guess:" + userGuess);
-                System.out.println(", Result:" + result);
+                prompt("Guess:" + userGuess);
+                prompt(", Result:" + result + "\n");
                 times = times + 1;
             } while(!result.equals("Kill"));
         } catch (Exception e) {
-            System.out.println(e);
+            prompt(e.toString());
+            prompt("\n");
         }
         System.out.println("You took " + times + " guesses");
     }
