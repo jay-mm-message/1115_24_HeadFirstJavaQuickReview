a456804931b0ad1857621c906c559ae3681a55cd
jay-mm-message <jay.mm.message@gmail.com>
2024-12-06 14:52:48 +0800

    delete old TcpSocket

--- ch15/TcpSocket/ClientTestDrive.java
+++
@@ -1,8 +0,0 @@
-package ch15.TcpSocket;
-
-public class ClientTestDrive {
-    public static void main(String[] args) {
-        Client client = new Client();
-        client.go();
-    }
-}
--- ch15/TcpSocket/ServerTestDrive.java
+++
@@ -1,8 +0,0 @@
-package ch15.TcpSocket;
-
-public class ServerTestDrive {
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.go();
-    }
-}
