fd5f42b72c61113b1bda235c10ff61449ebb41ac
jay-mm-message <jay.mm.message@gmail.com>
2024-12-25 09:27:08 +0800

    Add command

--- ch02/DVDPlayer/DVDPlayer.java
+++ ch02/DVDPlayer/DVDPlayer.java
@@ -1,11 +1,25 @@
 package ch02.DVDPlayer;
 
 public class DVDPlayer {
+
+    /**
+     * Constructor and initialization a record state on DVD player
+     * @param canRecord canRecord is a record state
+     */
     boolean canRecord = false;
 
+    /**
+     * The method will show that DVD state
+     * @return It will show that DVD is playing
+     */
     void playDVD() {
         System.out.println("DVD playing");
     }
+
+    /**
+     * The method will show that DVD state
+     * @return It will show that DVD is recording
+     */
     void recordDVD() {
         System.out.println("DVD recording");
     }
